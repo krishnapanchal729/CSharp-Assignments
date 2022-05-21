0978ec587fb123c2deef1e33e3bbb4f06e12e406
Krishna <19tcs108.krishna.a60@gmail.com>
2022-05-21 14:26:25 +0530

    C# | MatrixGrid

---
+++ MatrixGrid/MatrixGridForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace MatrixGrid
+{
+    partial class MatrixGridForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem50 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem100 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem150 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem200 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem50,
+            this.toolStripMenuItem100,
+            this.toolStripMenuItem150,
+            this.toolStripMenuItem200});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // toolStripMenuItem50
+            // 
+            this.toolStripMenuItem50.Name = "toolStripMenuItem50";
+            this.toolStripMenuItem50.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem50.Text = "50%";
+            this.toolStripMenuItem50.Click += new System.EventHandler(this.toolStripMenuItem50_Click);
+            // 
+            // toolStripMenuItem100
+            // 
+            this.toolStripMenuItem100.Name = "toolStripMenuItem100";
+            this.toolStripMenuItem100.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem100.Text = "100%";
+            this.toolStripMenuItem100.Click += new System.EventHandler(this.toolStripMenuItem100_Click);
+            // 
+            // toolStripMenuItem150
+            // 
+            this.toolStripMenuItem150.Name = "toolStripMenuItem150";
+            this.toolStripMenuItem150.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem150.Text = "150%";
+            this.toolStripMenuItem150.Click += new System.EventHandler(this.toolStripMenuItem150_Click);
+            // 
+            // toolStripMenuItem200
+            // 
+            this.toolStripMenuItem200.Name = "toolStripMenuItem200";
+            this.toolStripMenuItem200.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem200.Text = "200%";
+            this.toolStripMenuItem200.Click += new System.EventHandler(this.toolStripMenuItem200_Click);
+            // 
+            // MatrixGridForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(742, 604);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MatrixGridForm";
+            this.Text = "Matrix Grid";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem50;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem100;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem150;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem200;
+    }
+}
+
---
+++ MatrixGrid/MatrixGridForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixGrid
+{
+    public partial class MatrixGridForm : Form
+    {
+        public int m_width;       // Width of the grid cell
+        public int m_Height;      // The height of the cell
+        public int m_NoOfRows;    // The Number of Rows
+        public int m_NoOfCols;    // The Number of Columns
+
+        public int m_XOffset;     //Offset from which drawing start
+        public int m_YOffset;
+
+        public const int DEFAULT_X_OFFSET = 100;
+        public const int DEFAULT_Y_OFFSET = 50;
+        public const int DEFAULT_NO_ROWS = 3;
+        public const int DEFAULT_NO_COLS = 3;
+        public const int DEFAULT_WIDTH = 60;
+        public const int DEFAULT_HEIGHT = 60;
+
+        public MatrixGridForm()
+        {
+            Initialize();
+            InitializeComponent();
+        }
+
+        private void OnPaint(object sender, EventArgs e)
+        {
+            DrawGrid();
+        }
+
+        public void Initialize()
+        {
+            //Put all the default values
+            m_NoOfRows = DEFAULT_NO_ROWS;
+            m_NoOfCols = DEFAULT_NO_COLS;
+            m_width = DEFAULT_WIDTH;
+            m_Height = DEFAULT_HEIGHT;
+            m_XOffset = DEFAULT_X_OFFSET;
+            m_YOffset = DEFAULT_Y_OFFSET;
+
+        }
+
+        private void DrawGrid()
+        {
+            Graphics boardLayout = this.CreateGraphics();
+            Pen LayoutPen = new Pen(Color.Purple);
+            LayoutPen.Width = 3;
+
+            //boardLayout.DrawLine(layoutPen, 0, 0, 100, 0);
+            int X = DEFAULT_X_OFFSET;
+            int Y = DEFAULT_Y_OFFSET;
+
+            //Drawing Rows
+            for(int i = 0; i <= m_NoOfRows; i++)
+            {
+                boardLayout.DrawLine(LayoutPen, X, Y, X + this.m_width * this.m_NoOfCols, Y);
+                Y = Y + m_Height;
+            }
+
+            //Drawing Cols
+            X = DEFAULT_X_OFFSET;
+            Y = DEFAULT_Y_OFFSET;
+            for(int j = 0; j <= m_NoOfCols; j++)
+            {
+                boardLayout.DrawLine(LayoutPen, X, Y, X, Y + this.m_Height * this.m_NoOfRows);
+                X = X + this.m_width;
+            }
+
+        }
+
+        private void toolStripMenuItem50_Click(object sender, EventArgs e)
+        {
+            m_width = DEFAULT_WIDTH / 2;
+            m_Height = DEFAULT_HEIGHT / 2;
+            this.Refresh();
+        }
+
+        private void toolStripMenuItem100_Click(object sender, EventArgs e)
+        {
+            m_width = DEFAULT_WIDTH;
+            m_Height = DEFAULT_HEIGHT;
+            this.Refresh(); //Call OnPaint
+        }
+
+        private void toolStripMenuItem150_Click(object sender, EventArgs e)
+        {
+            m_width = DEFAULT_WIDTH * 2;
+            m_Height = DEFAULT_HEIGHT * 2;
+            this.Refresh(); //Call OnPaint
+        } 
+
+        private void toolStripMenuItem200_Click(object sender, EventArgs e)
+        {
+            m_width = DEFAULT_WIDTH * 3;
+            m_Height = DEFAULT_HEIGHT * 3;
+            this.Refresh(); //Call OnPaint
+        }
+    }
+}
