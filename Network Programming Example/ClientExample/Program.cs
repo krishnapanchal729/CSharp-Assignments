db3949bd778c5c6a5b7aa9ff477b5c6b93bb6cd8
Krishna <19tcs108.krishna.a60@gmail.com>
2022-05-21 14:24:48 +0530

    C# | Client/Server

---
+++ Network Programming Example/ClientExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ClientExample
+{
+    class Program
+    {
+        [DllImport("msvcrt")]
+        static extern int _getch();
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcplnt = new TcpClient();
+                Console.WriteLine("Connecting....");
+
+                tcplnt.Connect("127.0.0.1", 8001); // use the same ipaddress as in the server progra
+
+                Console.WriteLine("Connected");
+                Console.WriteLine("Enter the Message to be Transmitted:");
+
+                //read the contents from console
+                String str = Console.ReadLine();
+                Stream stm = tcplnt.GetStream();
+
+                //send it to the server as ASCII text encoding
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcplnt.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error....." + e.StackTrace);
+            }
+            _getch();
+        }
+    }
+}
---
+++ Network Programming Example/ServerExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress ipAdd = IPAddress.Parse("127.0.0.1"); // use local m/c ip address
+
+                /* Initializes the listener */
+                TcpListener myList = new TcpListener(ipAdd, 8001);
+
+                /* Start Listening at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001");
+                Console.WriteLine("The local End-point id :" + myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection....");
+
+                //Accepts the connection
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection Accepted from" + s.RemoteEndPoint);
+
+                //Get the data
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Received...");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(b[i]));
+
+                //Send the Replay
+                ASCIIEncoding asen = new ASCIIEncoding();
+                s.Send(asen.GetBytes("The message was received by the server."));
+                Console.WriteLine("\nSent Acknowledgement");
+
+                //Clean Up
+                s.Close();
+                myList.Stop();
+            }
+            catch ( Exception e)
+            {
+                Console.WriteLine("Error......" + e.StackTrace);
+            }
+        }
+    }
+}
